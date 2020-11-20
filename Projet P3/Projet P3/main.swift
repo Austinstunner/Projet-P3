707831d808e4a185ae46c10928a41c137f404d80
AT <anthony.tuffery85@orange.fr>
2020-11-20 01:00:59 +0100

    Réinitialisation du Projet P3
    -Reset de tous les fichiers .Swift
    -Ajout des fichiers Game.Swift & Player.Swift

--- Projet P3/Projet P3/main.swift
+++ Projet P3/Projet P3/main.swift
@@ -1,19 +1,1 @@
-// Test du code
-home()
-
-
-/* print("tu t'appelle comment ?")
-
- if let réponse = readLine() {
-    print(" Je m'appelle \(réponse) !")
-
-    thrall.name = réponse
-} */
-
-
-
-
-// Fin test du code
-
-firstStep()
 
