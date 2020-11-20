1ae99e05da04b08d5ba2ece06bd59d714721a1d8
AT <anthony.tuffery85@orange.fr>
2020-11-20 02:33:40 +0100

    Projet P3 Ver 1.0
    Edit des fichiers "Character.Swift" & "Weapon.Swift" :
    
    [Character.Swift] : 
    
    - Création de la classe mère "Character"
    - Création des classes héritées "KeyGuardian", "Dps" & "Tank"
    --------
    [Weapon.Swift] : 
    
    - Création de la classe mère "Weapon"

--- Projet P3/Projet P3/Weapon.swift
+++ Projet P3/Projet P3/Weapon.swift
@@ -1,1 +1,13 @@
+//Classe Mère
 
+class Weapon {
+    var name: String = ""
+    var type: String = ""
+    var damage: Int = 0
+    
+    init(name: String, type : String, damage: Int) {
+        self.name = name
+        self.type = type
+        self.damage = damage
+    }
+}
